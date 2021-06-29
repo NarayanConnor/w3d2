1c213a5d63ad2da39e39c310385615c5baaab09a
David Woolner <davidwoolner@gmail.com>
2021-06-29 12:18:40 -0400

    add card class

---
+++ card.rb
@@ -0,0 +1,27 @@
+class Card
+
+    def initialize(face) 
+        @face_value = face  #boolean
+        @face_down = true 
+    end
+
+    def hide
+        if !@face_down 
+            @face_down = true 
+        else  
+            @face_down = false 
+        end 
+    end
+
+    def reveal
+        @face_value 
+    end
+
+    def to_s
+        @face_value.to_s 
+    end
+
+    def ==(card)
+        self.face_value == card.face_value 
+    end
+end
